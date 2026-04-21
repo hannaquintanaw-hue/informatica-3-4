e147d4bb7127f92c70079c9f0cf5ca98b49aed5c
hannaquintanaw-hue <hannaquintana.w@gmail.com>
2026-04-21 21:58:12 +0000

    hello

---
+++ naming_variables.c
@@ -0,0 +1,9 @@
+int number;
+// I fixed the variable by eliminating the signo de exclamacion.
+int test_value;
+// I fixed the variable by including a underscore between text and value.
+int example1;
+// I fixed this variable by deliting the 1 and put it at last.
+float hanna;
+// I deleted the int because float and int cannot go together 
+printf("Variables created successfully.\n");
