0332fd1564f4f66d2ab4385f0eaa76d432f1e25a
hannaquintanaw-hue <hannaquintana.w@gmail.com>
2026-04-24 17:28:09 +0000

    hello

--- hello.c
+++ hello.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 int main(void)
 {
-    char yourname[50];
+    char yourname[80];
     printf("Enter you name: ");
     scanf("%s", &yourname);
-    
+    printf("Hello, %s! \n", yourname);
+    char color[80];
+    printf("Enter your favorite color: ");
+    scanf("%s", &color);
+    printf("Your favorite color is, %s!\n", color);
+
+}
